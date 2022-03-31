5b33cbfa951755e60d34aa6976a6dca81ebe40f7
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-03-31 08:53:16 +0530

    Modelling...

--- lib/model/location.dart
+++ lib/model/location.dart
@@ -1,17 +1,56 @@
+import 'dart:convert';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 class UserLocation {
-  double longitude = 88.3056119;
-  double latitude = 22.5892652;
+  String apiKey = 'Secret Key';
+  double lat = 40.7128;
+  double lon = 74.0060;
 
   Future<void> getLocationWithGeolocator() async {
     try {
       Position position = await Geolocator.getCurrentPosition(
           desiredAccuracy: LocationAccuracy.lowest);
-      latitude = position.latitude;
-      longitude = position.longitude;
+      lat = position.latitude;
+      lon = position.longitude;
     } catch (e) {
       print(e);
+    }
+  }
+
+  Future<String> getCityName() async {
+    final httpsUri = Uri.http('api.openweathermap.org', '/data/2.5/weather', {
+      'lat': '$lat',
+      'lon': '$lon',
+      'appid': apiKey,
+    });
+
+    var request = await http.get(httpsUri);
+    if (request.statusCode == 200) {
+      String data = request.body.toString();
+      var city = jsonDecode(data)['name'];
+
+      return city;
+    } else {
+      return '${request.statusCode}';
+    }
+  }
+
+  Future<String> getWeather() async {
+    final httpsUri = Uri.http('api.openweathermap.org', '/data/2.5/weather', {
+      'lat': '$lat',
+      'lon': '$lon',
+      'appid': apiKey,
+    });
+
+    var request = await http.get(httpsUri);
+    if (request.statusCode == 200) {
+      String data = request.body.toString();
+      var description = jsonDecode(data)['weather'][0]['description'];
+
+      return description;
+    } else {
+      return '${request.statusCode}';
     }
   }
 }
--- lib/view/my_app_home.dart
+++ lib/view/my_app_home.dart
@@ -1,7 +1,6 @@
-import 'dart:convert';
-import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+
+import '../model/location.dart';
 
 class MyAppHome extends StatefulWidget {
   const MyAppHome({Key? key, required this.title}) : super(key: key);
@@ -13,52 +12,13 @@ class MyAppHome extends StatefulWidget {
 }
 
 class _MyAppHomeState extends State<MyAppHome> {
-  String apiKey = 'Your Key';
-  double lat = 40.7128;
-  double lon = 74.0060;
-
-  Future<String> getCityName() async {
-    final httpsUri = Uri.http('api.openweathermap.org', '/data/2.5/weather', {
-      'lat': '$lat',
-      'lon': '$lon',
-      'appid': apiKey,
-    });
-
-    var request = await http.get(httpsUri);
-    if (request.statusCode == 200) {
-      String data = request.body.toString();
-      var city = jsonDecode(data)['name'];
-
-      return city;
-    } else {
-      return '${request.statusCode}';
-    }
-  }
-
-  Future<String> getWeather() async {
-    final httpsUri = Uri.http('api.openweathermap.org', '/data/2.5/weather', {
-      'lat': '$lat',
-      'lon': '$lon',
-      'appid': apiKey,
-    });
-
-    var request = await http.get(httpsUri);
-    if (request.statusCode == 200) {
-      String data = request.body.toString();
-      var description = jsonDecode(data)['weather'][0]['description'];
-
-      return description;
-    } else {
-      return '${request.statusCode}';
-    }
-  }
-
-  String city = 'Your ';
-  String description = 'Good Weather ';
+  var city = 'Your ';
+  var description = 'Good Weather ';
 
   @override
   Widget build(BuildContext context) {
-    //var city = getData();
+    UserLocation location = UserLocation();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Get City and Weather'),
@@ -80,12 +40,12 @@ class _MyAppHomeState extends State<MyAppHome> {
             ),
             TextButton(
               onPressed: () {
-                getCityName().then((String result) {
+                location.getCityName().then((result) {
                   setState(() {
                     city = result;
                   });
                 });
-                getWeather().then((String result) {
+                location.getWeather().then((result) {
                   setState(() {
                     description = result;
                   });
